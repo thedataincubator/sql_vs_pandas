fc1b3f347513baae8a377a7b2676f277db0577db
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2016-07-03 20:29:14 -0400

    csv generation

--- gen/gen_csv.py
+++ gen/gen_csv.py
@@ -1,5 +1,7 @@
 import random
 import string
+import sys
+import csv
 
 def random_string(length, chars=string.ascii_letters):
   return ''.join(random.choice(chars) for _ in xrange(length))
@@ -10,9 +12,6 @@ def gen_csv(N):
     yield (random_string(8), random_string(1, chars='abcdefg'), random.randint(1900, 2000), random.uniform(1e4, 1e5))
 
 if __name__ == "__main__":
-  import sys
-  import csv
-
   length = int(sys.argv[1])
   with open(sys.argv[2], "w") as fh:
     writer = csv.writer(fh)
